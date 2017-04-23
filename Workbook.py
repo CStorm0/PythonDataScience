9ec2b6d000b54f061c0193dcd44a5a00599587b7
CStorm0 <christian.storm93@gmail.com>
2017-04-23 19:10:20 +0100

    Drafted get_list_of_university_towns()
    get_list_of_university_towns() loads and formats university_towns.txt

--- Workbook.py
+++ Workbook.py
@@ -11,7 +11,7 @@ from scipy.stats import ttest_ind
 # Use this dictionary to map state names to two letter acronyms
 states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
 
-UniversityTowns_raw = pd.read_table(filepath_or_buffer = 'university_towns.txt', header=None)
+UniversityTowns_raw = pd.read_table(filepath_or_buffer = "F:/GITHub/Coursera/Intro to Data Science in Python/IntroToDataScienceInPython/university_towns.txt", header=None)
 UniversityTowns_raw.rename(columns={0:'Area'}, inplace=True)
 
 stateIndices=UniversityTowns_raw[UniversityTowns_raw['Area'].str.contains('edit')].index
@@ -29,7 +29,8 @@ for region in UniversityTowns_raw.Area:
 UniversityTowns = pd.DataFrame(data={'State':stateList, 'RegionName':cityList})
 UniversityTowns.State = UniversityTowns.State.apply(lambda state: state.rstrip('[edit]'))
 UniversityTowns.RegionName = UniversityTowns.RegionName.apply(lambda region: region.split('(')[0])
-    
+
+UniversityTowns = UniversityTowns[['State', 'RegionName']]
 
 def get_list_of_university_towns():
     '''Returns a DataFrame of towns and the states they are in from the 
