a280c8284390e74b6c97d7793da8730ef7963e92
CStorm0 <christian.storm93@gmail.com>
2017-04-23 19:26:58 +0100

    Updates to get_list_of_university_towns()
    Functional now

--- Workbook.py
+++ Workbook.py
@@ -11,7 +11,7 @@ from scipy.stats import ttest_ind
 # Use this dictionary to map state names to two letter acronyms
 states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
 
-UniversityTowns_raw = pd.read_table(filepath_or_buffer = 'university_towns.txt', header=None)
+UniversityTowns_raw = pd.read_table(filepath_or_buffer = 'F:\GITHub\Coursera\Intro to Data Science in Python\IntroToDataScienceInPython/university_towns.txt', header=None)
 UniversityTowns_raw.rename(columns={0:'Area'}, inplace=True)
 
 stateIndices=UniversityTowns_raw[UniversityTowns_raw['Area'].str.contains('edit')].index
@@ -21,14 +21,15 @@ stateList = []
 cityList = []
 for region in UniversityTowns_raw.Area:
     if 'edit' in region:
-        currentState = region        
+        currentState = region   
     else:
         cityList.append(region)
         stateList.append(currentState)
 
 UniversityTowns = pd.DataFrame(data={'State':stateList, 'RegionName':cityList})
-UniversityTowns.State = UniversityTowns.State.apply(lambda state: state.rstrip('[edit]'))
+UniversityTowns.State = UniversityTowns.State.apply(lambda state: state.rstrip('edit]').rstrip('['))
 UniversityTowns.RegionName = UniversityTowns.RegionName.apply(lambda region: region.split('(')[0])
+UniversityTowns.RegionName = UniversityTowns.RegionName.apply(lambda region: region.rstrip(' '))
     
 
 def get_list_of_university_towns():
@@ -43,4 +44,24 @@ def get_list_of_university_towns():
     2. For "RegionName", when applicable, removing every character from " (" to the end.
     3. Depending on how you read the data, you may need to remove newline character '\n'. '''
     
-    return "ANSWER"
+    UniversityTowns_raw = pd.read_table(filepath_or_buffer = "university_towns.txt", header=None)
+    UniversityTowns_raw.rename(columns={0:'Area'}, inplace=True)
+
+    stateList = []
+    cityList = []
+    for region in UniversityTowns_raw.Area:
+        if 'edit' in region:
+            currentState = region        
+        else:
+            cityList.append(region)
+            stateList.append(currentState)
+
+    UniversityTowns = pd.DataFrame(data={'State':stateList, 'RegionName':cityList})
+    UniversityTowns.State = UniversityTowns.State.apply(lambda state: state.rstrip('edit]').rstrip('['))
+    UniversityTowns.RegionName = UniversityTowns.RegionName.apply(lambda region: region.split('(')[0])
+    UniversityTowns.RegionName = UniversityTowns.RegionName.apply(lambda region: region.rstrip(' '))
+
+    UniversityTowns = UniversityTowns[['State', 'RegionName']]
+    
+    return UniversityTowns
+get_list_of_university_towns()
