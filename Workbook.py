a255253a72f7fcd3a5bfd39cdb73040de674b373
CStorm0 <christian.storm93@gmail.com>
2017-04-24 22:17:46 +0100

    Created function to get quarterly data for house prices
    Halfway there, should map State codes to State names

--- Workbook.py
+++ Workbook.py
@@ -14,8 +14,8 @@ states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada'
 UniversityTowns_raw = pd.read_table(filepath_or_buffer = 'F:\GITHub\Coursera\Intro to Data Science in Python\IntroToDataScienceInPython/university_towns.txt', header=None)
 UniversityTowns_raw.rename(columns={0:'Area'}, inplace=True)
 
-stateIndices=UniversityTowns_raw[UniversityTowns_raw['Area'].str.contains('edit')].index
-stateIndicesList=stateIndices.tolist()
+#stateIndices=UniversityTowns_raw[UniversityTowns_raw['Area'].str.contains('edit')].index
+#stateIndicesList=stateIndices.tolist()
 
 stateList = []
 cityList = []
@@ -65,3 +65,98 @@ def get_list_of_university_towns():
     
     return UniversityTowns
 get_list_of_university_towns()
+
+
+
+
+GDP_raw = pd.read_excel('gdplev.xls')
+GDP = GDP_raw[['Unnamed: 4','Unnamed: 6']][219:]
+GDP.rename(columns={'Unnamed: 4':'Quarter', 'Unnamed: 6':'GDP'}, inplace=True)
+
+GDP['Change']=GDP.GDP-GDP.GDP.shift(1)
+GDP['Next_Change']=GDP.Change.shift(-1)
+GDP[(GDP.Change<0) & (GDP.Next_Change<0)]
+GDP['Recession']=False
+GDP.set_value(GDP[(GDP.Change<0) & (GDP.Next_Change<0)].index.tolist(), 'Recession', True)
+
+
+def get_recession_start():
+    '''Returns the year and quarter of the recession start time as a 
+    string value in a format such as 2005q3'''
+    
+    GDP_raw = pd.read_excel('gdplev.xls')
+    GDP = GDP_raw[['Unnamed: 4','Unnamed: 6']][219:]
+    GDP.rename(columns={'Unnamed: 4':'Quarter', 'Unnamed: 6':'GDP'}, inplace=True)
+
+    GDP['Change']=GDP.GDP-GDP.GDP.shift(1)
+    GDP['Next_Change']=GDP.Change.shift(-1)
+    GDP[(GDP.Change<0) & (GDP.Next_Change<0)]
+    GDP['Recession']=False
+    GDP.set_value(GDP[(GDP.Change<0) & (GDP.Next_Change<0)].index.tolist(), 'Recession', True)
+    
+    recession_start = GDP.Quarter[(GDP.Change<0) & (GDP.Next_Change<0)].iloc[0]
+    
+    return recession_start
+get_recession_start()
+
+def get_recession_end():
+    '''Returns the year and quarter of the recession end time as a 
+    string value in a format such as 2005q3'''
+    GDP_raw = pd.read_excel('gdplev.xls')
+    GDP = GDP_raw[['Unnamed: 4','Unnamed: 6']][219:]
+    GDP.rename(columns={'Unnamed: 4':'Quarter', 'Unnamed: 6':'GDP'}, inplace=True)
+
+    GDP['Change']=GDP.GDP-GDP.GDP.shift(1)
+    GDP['Previous_Change']=GDP.Change.shift(1)
+    GDP['Next_Change']=GDP.Change.shift(-1)
+
+    GDP['Recession']=False
+    GDP.set_value(GDP[(GDP.Change<0) & ((GDP.Next_Change<0)|(GDP.Previous_Change<0))].index.tolist(), 'Recession', True)
+               
+    last_recession_month = GDP[GDP['Recession']==True].iloc[-1].Quarter
+    recession_end = GDP.loc[GDP[GDP['Quarter']==last_recession_month].index.tolist()[0]+2].Quarter 
+        
+    return recession_end
+get_recession_end()
+
+def get_recession_bottom():
+    '''Returns the year and quarter of the recession bottom time as a 
+    string value in a format such as 2005q3'''
+    GDP_raw = pd.read_excel('gdplev.xls')
+    GDP = GDP_raw[['Unnamed: 4','Unnamed: 6']][219:]
+    GDP.rename(columns={'Unnamed: 4':'Quarter', 'Unnamed: 6':'GDP'}, inplace=True)
+
+    GDP['Change']=GDP.GDP-GDP.GDP.shift(1)
+    GDP['Previous_Change']=GDP.Change.shift(1)
+    GDP['Next_Change']=GDP.Change.shift(-1)
+
+    GDP['Recession']=False
+    GDP.set_value(GDP[(GDP.Change<0) & ((GDP.Next_Change<0)|(GDP.Previous_Change<0))].index.tolist(), 'Recession', True) 
+                
+    recession_bottom = GDP[GDP['Recession']==True].sort_values('GDP').iloc[0].Quarter
+    
+    return recession_bottom
+get_recession_bottom()
+
+
+Housing_raw = pd.read_csv('City_Zhvi_AllHomes.csv')
+Housing = Housing_raw.drop(Housing_raw.columns[6:51], axis = 1)
+
+Housing = Housing.set_index(['State', 'RegionName'])
+
+#Housing.groupby(pd.PeriodIndex(Housing.columns, freq='Q'), axis=1).mean()
+
+HousingPriceMonthly = Housing[Housing.columns[4:205]]
+HousingPriceQuarterly=HousingPriceMonthly.groupby(pd.PeriodIndex(HousingPriceMonthly.columns, freq='Q'), axis=1).mean()
+
+StringColumns = HousingPriceQuarterly.columns.to_series().astype(str).reset_index().drop(0, axis=1)
+    
+StringColumns = HousingPriceQuarterly.columns.format()
+HousingPriceQuarterly.columns = [x.lower() for x in StringColumns]
+    
+
+
+
+
+
+
